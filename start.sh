f2b6b741506e32e835b013b3ffbe3a9b3cda85bb
Niels Højen <niels@hojen.net>
2019-01-03 14:36:58 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if [ -f /reposado/code/preferences.plist ]; then
+  /bin/cp /reposado/code/preferences.plist /margarita/preferences.plis
+fi
+
+: "${APACHE_CONFDIR:=/etc/apache2}"
+: "${APACHE_ENVVARS:=$APACHE_CONFDIR/envvars}"
+if test -f "$APACHE_ENVVARS"; then
+	. "$APACHE_ENVVARS"
+fi
+
+# Apache gets grumpy about PID files pre-existing
+: "${APACHE_PID_FILE:=${APACHE_RUN_DIR:=/var/run/apache2}/apache2.pid}"
+rm -f "$APACHE_PID_FILE"
+
+exec apache2 -DFOREGROUND "$@"
